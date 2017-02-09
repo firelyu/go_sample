c6173b2a5891eeca20432840648ea8e1f04e92c7
Liao Yu <liaoyu17@gmail.com>
2017-02-09 11:32:12 +0800

    Redirect / to /view/frontpage.

--- web/exec-web.go
+++ web/exec-web.go
@@ -1,36 +1,37 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"net/http"
 	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"regexp"
 )
 
 const (
-	SUFFIX = ".txt"
-	PERMISSION = 0600
-	LISTEN_PORT = ":8080"
+	SUFFIX       = ".txt"
+	PERMISSION   = 0600
+	LISTEN_PORT  = ":8080"
 	TEMPLATE_DIR = "web/tmpl"
-	FILE_DIR = "web/data"
+	FILE_DIR     = "web/data"
+	HOMEPAGE     = "frontpage"
 )
 
 var (
-	templates *template.Template
+	templates  *template.Template
 	validPaths *regexp.Regexp
 )
 
 type Page struct {
-	Title	string
-	Body	[]byte
+	Title string
+	Body  []byte
 }
 
 // Save the body to file
-func (p *Page) save() error  {
+func (p *Page) save() error {
 	filename := p.Title + SUFFIX
 	permission := os.FileMode(PERMISSION)
-	return ioutil.WriteFile(FILE_DIR + "/" + filename, p.Body, permission)
+	return ioutil.WriteFile(FILE_DIR+"/"+filename, p.Body, permission)
 }
 
 // Read body content from file
@@ -40,12 +41,12 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title:title, Body:body}, nil
+	return &Page{Title: title, Body: body}, nil
 }
 
 // Read the template from the templates cache
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page)  {
-	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -67,7 +68,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/" + title, http.StatusNotFound)
+		http.Redirect(w, r, "/edit/"+title, http.StatusNotFound)
 		return
 	}
 	renderTemplate(w, "view", p)
@@ -76,7 +77,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
-		p = &Page{Title:title}
+		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
 
@@ -84,7 +85,7 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	p := &Page{Title:title, Body:[]byte(body)}
+	p := &Page{Title: title, Body: []byte(body)}
 
 	err := p.save()
 	if err != nil {
@@ -92,14 +93,18 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 		return
 	}
 
-	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/"+HOMEPAGE, http.StatusFound)
 }
 
 func main() {
 	// Cache the templates
 	templates = template.Must(template.ParseFiles(
-		TEMPLATE_DIR + "/edit.html",
-		TEMPLATE_DIR + "/view.html",
+		TEMPLATE_DIR+"/edit.html",
+		TEMPLATE_DIR+"/view.html",
 	))
 
 	validPaths = regexp.MustCompile("^/(edit|view|save)/([a-zA-Z0-9]*)$")
@@ -107,5 +112,6 @@ func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", homeHandler)
 	http.ListenAndServe(LISTEN_PORT, nil)
 }
