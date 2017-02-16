f6e2174cc2d0f2b81d8c6de4f0ffbb114cdcb330
Liao Yu <liaoyu17@gmail.com>
2017-02-16 14:29:30 +0800

    The jobs channel store the ptr.

--- goroutine-pool/pool.go
+++ goroutine-pool/pool.go
@@ -7,15 +7,24 @@ import (
 	"time"
 )
 
-func worker(id int, job chan int, result chan bool) {
+type Job struct {
+	JobId int
+}
+
+func worker(id int, job chan *Job, result chan bool) {
 	go func() {
 		for {
 			fmt.Println("Waiting for job...")
 			select {
 			case j := <-job:
-				fmt.Println("worker", id, "started  job", j)
+				// The job channel is closed.
+				if j == nil {
+					//fmt.Println("The channel is close", j)
+					return
+				}
+				fmt.Println("worker", id, "started  job", j.JobId)
 				time.Sleep(time.Second)
-				fmt.Println("worker", id, "finished job", j)
+				fmt.Println("worker", id, "finished job", j.JobId)
 				result <- true
 			}
 		}
@@ -23,30 +32,38 @@ func worker(id int, job chan int, result chan bool) {
 }
 
 const (
-	workerNumber        = 3
+	workerNumber        = 5
 	jobNumber           = 10
 	jobsChannelCache    = 100
 	resultsChannelCache = 100
 )
 
 func main() {
-	jobs := make(chan int, jobsChannelCache)
+	// According to the following link, it is better to set the job channel as pointer channel.
+	// So after close the channel. The j:= <jobs get nil ptr.
+	// http://stackoverflow.com/questions/28447297/how-to-check-for-an-empty-struct/#answer-28449449
+	jobs := make(chan *Job, jobsChannelCache)
 	results := make(chan bool, resultsChannelCache)
 
 	fmt.Println("len(jobs)", len(jobs))
 
+	// Star worker goroutines.
 	for i := 0; i < workerNumber; i++ {
 		worker(i, jobs, results)
 	}
 
+	// Send job to the channel.
 	time.Sleep(time.Second)
 	for j := 0; j < jobNumber; j++ {
-		jobs <- j
+		jobs <- &Job{JobId:j}
 	}
-	//close(jobs)
+	close(jobs)
 
-	for len(jobs) != 0 || len(results) != jobNumber {
+	for len(jobs) != 0 || len(results) != jobNumber{
 		time.Sleep(100 * time.Millisecond)
 	}
-	fmt.Println("len(jobs)", len(jobs))
+	fmt.Printf("len(jobs) is %d, len(results) is %d\n", len(jobs), len(results))
+
+	fmt.Println("Complete main.")
+
 }
