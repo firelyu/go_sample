7b82e3d3e55f1cbf3de7f544c0bf0df61b76b844
Liao Yu <liaoyu17@gmail.com>
2017-02-25 13:15:08 +0800

    Add the random package.

---
+++ random/bytes.go
@@ -0,0 +1,102 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+//http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang#answer-31832326
+
+var (
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+const (
+	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func RandStringBytesReminder(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63() % int64(len(letterBytes))]
+	}
+	return string(b)
+}
+
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+)
+
+func RandStringBytesMask(n int) string {
+	b := make([]byte, n)
+	for i := 0; i < n; {
+		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i++
+		}
+	}
+	return string(b)
+}
+
+const (
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandStringBytesMaskImpr(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func RandStringBytesMaskImprSrc(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
---
+++ random/bytes_test.go
@@ -0,0 +1,91 @@
+package random_test
+
+import (
+	. "github.com/firelyu/gotour/random"
+	"strconv"
+	"testing"
+)
+
+var lengthList = []int{128, 256, 512, 1024, 2048}
+
+func benchmarkRand(fn func(int) string, length int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fn(length)
+	}
+}
+
+func benchmarkRandGroup(length int, b *testing.B) {
+	b.Run("RandStringRunes"+strconv.Itoa(length), func(b *testing.B) {
+		benchmarkRand(RandStringRunes, length, b)
+	})
+	b.Run("RandStringBytes"+strconv.Itoa(length), func(b *testing.B) {
+		benchmarkRand(RandStringBytes, length, b)
+	})
+	b.Run("RandStringBytesReminder"+strconv.Itoa(length), func(b *testing.B) {
+		benchmarkRand(RandStringBytesReminder, length, b)
+	})
+	b.Run("RandStringBytesMask"+strconv.Itoa(length), func(b *testing.B) {
+		benchmarkRand(RandStringBytesMask, length, b)
+	})
+	b.Run("RandStringBytesMaskImpr"+strconv.Itoa(length), func(b *testing.B) {
+		benchmarkRand(RandStringBytesMaskImpr, length, b)
+	})
+	b.Run("RandStringBytesMaskImprSrc"+strconv.Itoa(length), func(b *testing.B) {
+		benchmarkRand(RandStringBytesMaskImprSrc, length, b)
+	})
+}
+
+func BenchmarkMain(b *testing.B) {
+	for _, len := range lengthList {
+		benchmarkRandGroup(len, b)
+	}
+}
+
+//func BenchmarkRandStringRunes(b *testing.B)  {
+//	for _, length := range lengthList {
+//		b.Run(strconv.Itoa(length), func(b *testing.B) {
+//			benchmarkRand(RandStringRunes, length, b)
+//		})
+//	}
+//
+//}
+//
+//func BenchmarkRandStringBytes(b *testing.B)  {
+//	for _, length := range lengthList {
+//		b.Run(strconv.Itoa(length), func(b *testing.B) {
+//			benchmarkRand(RandStringBytes, length, b)
+//		})
+//	}
+//}
+//
+//func BenchmarkRandStringBytesReminder(b *testing.B)  {
+//	for _, length := range lengthList {
+//		b.Run(strconv.Itoa(length), func(b *testing.B) {
+//			benchmarkRand(RandStringBytesReminder, length, b)
+//		})
+//	}
+//}
+//
+//func BenchmarkRandStringBytesMask(b *testing.B)  {
+//	for _, length := range lengthList {
+//		b.Run(strconv.Itoa(length), func(b *testing.B) {
+//			benchmarkRand(RandStringBytesMask, length, b)
+//		})
+//	}
+//}
+//
+//func BenchmarkRandStringBytesMaskImpr(b *testing.B)  {
+//	for _, length := range lengthList {
+//		b.Run(strconv.Itoa(length), func(b *testing.B) {
+//			benchmarkRand(RandStringBytesMaskImpr, length, b)
+//		})
+//	}
+//}
+//
+//func BenchmarkRandStringBytesMaskImprSrc(b *testing.B)  {
+//	for _, length := range lengthList {
+//		b.Run(strconv.Itoa(length), func(b *testing.B) {
+//			benchmarkRand(RandStringBytesMaskImprSrc, length, b)
+//		})
+//	}
+//}
